0810ab2cf6c173d5bf191d008f7e3fa639c1a9bb
Jo Murgel <jo@rehabmedia.com>
2025-10-17 13:23:58 -0600

    adds embed class (wip) to handle embed logic

---
+++ inc/class-embed.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * RMG Premium Listings Embed Handler
+ *
+ * Handles embed template rendering and rewrite rules.
+ *
+ * @package RMG_Premium_Listings
+ */
+
+namespace RMG_Premium_Listings;
+
+// Prevent direct access.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Embed Class
+ */
+class Embed {}
