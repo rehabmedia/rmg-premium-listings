8a7c24de9fb0f94dae02b427f4f3cef0cfe815dd
Jo Murgel <jo@rehabmedia.com>
2025-10-17 13:24:35 -0600

    adds template for embed output (wip)

---
+++ templates/embed-listing-cards.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Embed Template for Listing Cards
+ *
+ * Minimal HTML template for iframe embedding.
+ * Variables available: $config, $render_args, $referrer, $state, $city
+ *
+ * @package RMG_Premium_Listings
+ */
+
+// Prevent direct access.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
